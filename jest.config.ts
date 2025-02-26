6f051f659df06d3c98feca24bed6a5c3f25542b1
Allan Legemaate <alegemaate@gmail.com>
2025-02-26 17:48:08 -0500

    fix: coverage report (#5)

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@ const config: Config = {
   verbose: true,
   testEnvironment: "node",
   testMatch: ["**/*.spec.ts"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+    "!src/**/*.d.ts",
+    "!src/**/*.spec.ts",
+  ],
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
