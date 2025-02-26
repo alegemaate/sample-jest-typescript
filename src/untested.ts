e17473821b3ace6f94c7744fe07d34e1c2e60b78
Allan Legemaate <alegemaate@gmail.com>
2025-02-26 17:58:06 -0500

    chore: break more tests for fun

--- src/untested.ts
+++ src/untested.ts
@@ -6,3 +6,12 @@
  * @returns The result of dividing a by b.
  */
 export const divide = (a: number, b: number) => a / b;
+
+/**
+ * Powers a number to the given exponent.
+ *
+ * @param a The base number.
+ * @param b The exponent.
+ * @returns The result of raising a to the power of b.
+ */
+export const power = (a: number, b: number) => a ** b;
