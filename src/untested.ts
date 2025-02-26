d12d6653d08337b52fe47e16a9cc5aebcadf25b0
Allan Legemaate <alegemaate@gmail.com>
2025-02-26 17:47:18 -0500

    fix: coverage report

---
+++ src/untested.ts
@@ -0,0 +1,8 @@
+/**
+ * Divides two numbers.
+ *
+ * @param a The first number.
+ * @param b The second number.
+ * @returns The result of dividing a by b.
+ */
+export const divide = (a: number, b: number) => a / b;
